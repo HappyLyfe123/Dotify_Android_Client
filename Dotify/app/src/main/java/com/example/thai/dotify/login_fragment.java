37d4b560335f0b14a08a224f9826f17e16ae2fc5
HappyLyfe123 <visalhok123@gmail.com>
2018-09-22 19:44:52 -0700

    Create method the check account credentical

--- Dotify/app/src/main/java/com/example/thai/dotify/login_fragment.java
+++ Dotify/app/src/main/java/com/example/thai/dotify/login_fragment.java
@@ -5,9 +5,19 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
-public class login_fragment extends Fragment {
+public class login_fragment extends Fragment implements View.OnClickListener {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private TextView errorMessageTextView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,4 +31,57 @@ public class login_fragment extends Fragment {
         super.onDetach();
     }
 
+    /**
+     * Initializes the main components of the fragment
+     *
+     * @param savedInstanceState The saved instance of the fragment
+     */
+    @Override
+    public void onActivityCreated( Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Initialize all of the views
+        usernameEditText = getActivity().findViewById(R.id.user_name_edit_text);
+        passwordEditText = getActivity().findViewById(R.id.password_edit_text);
+        errorMessageTextView = getActivity().findViewById(R.id.error_text_view);
+        Button signInButton = getActivity().findViewById(R.id.sign_in_button);
+        Button signUpButton = getActivity().findViewById(R.id.sign_up_button);
+
+        //Set listeners for buttons
+        signInButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+    }
+
+    /**
+     * Gets called upon the user clicking an interactive item on screen
+     *
+     * @param view The current view
+     */
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button:
+                if(isValidCredential(usernameEditText.getText().toString(), passwordEditText.getText().toString()) == 1){
+                    errorMessageTextView.setText(R.string.empty_login_entry);
+                }
+                break;
+            case R.id.sign_up_button:
+                break;
+        }
+    }
+
+    /**
+     * Gets called upon the user clicking an interactive item on screen
+     *
+     * @param username The user username
+     * @param password The user password
+     */
+    private int isValidCredential(String username, String password){
+        int flagType = 0;
+
+        if(username.trim().isEmpty() || password.trim().isEmpty()){
+            flagType = 1;
+        }
+
+        return flagType;
+    }
 }
