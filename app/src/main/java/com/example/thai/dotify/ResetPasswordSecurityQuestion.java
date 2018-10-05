b003fb926c1a358d273797d5e0027bb3a732b6b3
happylyfe123 <visalhok123@gmail.com>
2018-10-05 15:14:06 -0700

    Create an error box for username

--- app/src/main/java/com/example/thai/dotify/ResetPasswordSecurityQuestion.java
+++ app/src/main/java/com/example/thai/dotify/ResetPasswordSecurityQuestion.java
@@ -21,7 +21,7 @@ public class ResetPasswordSecurityQuestion extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reset_password_security_question, container, false);
+        return inflater.inflate(R.layout.reset_password_security_question_layout, container, false);
     }
 
     /**
--- app/src/main/java/com/example/thai/dotify/ResetPasswordUsernameFragment.java
+++ app/src/main/java/com/example/thai/dotify/ResetPasswordUsernameFragment.java
@@ -22,7 +22,7 @@ public class ResetPasswordUsernameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reset_password_username, container, false);
+        return inflater.inflate(R.layout.reset_password_username_layout, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
