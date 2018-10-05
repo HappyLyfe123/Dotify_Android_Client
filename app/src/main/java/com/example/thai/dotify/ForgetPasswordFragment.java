f1d574a9b944eb7de4a63c39eec3ec025bb89851
HappyLyfe123 <visalhok123@gmail.com>
2018-10-05 11:53:23 -0700

    Update forget password

--- app/src/main/java/com/example/thai/dotify/ForgetPasswordFragment.java
+++ app/src/main/java/com/example/thai/dotify/ForgetPasswordFragment.java
@@ -1,20 +1,31 @@
 package com.example.thai.dotify;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.widget.Button;
 
 public class ForgetPasswordFragment extends Fragment {
 
     private OnChangeFragmentListener onChangeFragmentListener;
+    private Button usernameSubmitButton;
 
+    ViewStub userNameStub, securityQuestionStub, resetPasswordStub;
 
     public interface OnChangeFragmentListener {
         void buttonClicked(StartUpContainer.AuthFragmentType fragmentType);
+    }
+
+    @Override
+    public void onInflate(Context context, AttributeSet attrs, Bundle savedInstanceState) {
+        super.onInflate(context, attrs, savedInstanceState);
     }
 
     /**
@@ -30,6 +41,15 @@ public class ForgetPasswordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forget_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_forget_password, container, false);
+
+        //
+        usernameSubmitButton = view.findViewById(R.id.submit_button);
+
+        userNameStub = view.findViewById(R.id.username_stub);
+        securityQuestionStub = view.findViewById(R.id.security_question_stub);
+        resetPasswordStub = view.findViewById(R.id.reset_password_stub);
+        View inflate = userNameStub.inflate();
+        return view;
     }
 }
