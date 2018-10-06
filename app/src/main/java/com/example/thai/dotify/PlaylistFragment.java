85c54cfbaf11a7772ac56aa8a0d906c4db2c12dd
Khoa100 <minhkhoavu954@gmail.com>
2018-10-06 15:10:43 -0700

    Add files via upload

--- app/src/main/java/com/example/thai/dotify/PlaylistFragment.java
+++ app/src/main/java/com/example/thai/dotify/PlaylistFragment.java
@@ -2,20 +2,30 @@ package com.example.thai.dotify;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.app.AlertDialog;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class PlaylistFragment extends Fragment {
+public class PlaylistFragment extends Fragment implements View.OnClickListener  {
 
     private View fragView;
+    private Button button;
+    private OnChangeFragmentListener onChangeFragmentListener;
 
     public static PlaylistFragment newInstance() {
         PlaylistFragment fragment = new PlaylistFragment();
         return fragment;
+    }
+
+    public interface OnChangeFragmentListener {
+        void buttonClicked(StartUpContainer.AuthFragmentType fragmentType);
     }
 
     @Override
@@ -31,6 +41,62 @@ public class PlaylistFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         fragView = inflater.inflate(R.layout.fragment_playlist, container, false);
         return fragView;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        button = getActivity().findViewById(R.id.create_playlist_button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.create_playlist_button: case R.id.delete_playlist_button:
+                AlertDialog dialogBox = createPlaylistDialog();
+                dialogBox.show();
+                dialogBox.cancel();
+                break;
+        }
+    }
+    /**
+     * Create an AlertDialog object to allow the user to create
+     *
+     * @return
+     */
+    private AlertDialog createPlaylistDialog() {
+        //Create an instance of the Alert Dialog
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        //Set the View of the Alert Dialog
+        final View alertDialogView = getActivity().getLayoutInflater().inflate(R.layout.fragment_create_playlist, null);
+        alertDialogBuilder.setView(alertDialogView);
+
+        //Initialize Views for this Fragment
+        final Button createPlaylist = (Button) alertDialogView.findViewById(R.id.create_button);
+        final TextView playlistText = (TextView) alertDialogView.findViewById(R.id.playlist_text_view);
+        final EditText playlistName = (EditText) alertDialogView.findViewById(R.id.playlist_edit_text);
+
+        //Set Listeners
+        createPlaylist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(playlistName.getText().length() > 0) {
+                   //add/delete playlist via backend service
+
+                }
+            }
+        });
+
+
+        return alertDialogBuilder.create();
+    }
+    /**
+     * Sets the OnChangeFragmentListener to communicate from this fragment to the activity
+     *
+     * @param onChangeFragmentListener The listener for communication
+     */
+    public void setOnChangeFragmentListener(OnChangeFragmentListener onChangeFragmentListener) {
+        this.onChangeFragmentListener = onChangeFragmentListener;
     }
 
 }
